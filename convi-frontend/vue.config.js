a08a8168ef2f8c4020697418fb27bc1d1acc8c0c
hara <hm.hzm331@gmail.com>
2024-05-11 09:28:56 +0900

    kokokoko

--- convi-frontend/vue.config.js
+++ convi-frontend/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: './docs',
+  outputDir: './dist',
   assetsDir: './',
   publicPath: './'
 })
